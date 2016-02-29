c2da8e7cd0afea991f551b522618cf6d47299402
Hannes Junnila <hannes.junnila@gmail.com>
2016-02-29 21:01:24 +0200

    Fix layout issues

--- app/components/Home.js
+++ app/components/Home.js
@@ -15,8 +15,8 @@ const Home = () => (
       </div>
       <CenterSelector/>
       <SizeSelector/>
-      <MapScaleSelector/>
       <DpiSelector/>
+      <MapScaleSelector/>
       <PixelScaleSelector/>
     </div>
     <div className={styles.main}>
