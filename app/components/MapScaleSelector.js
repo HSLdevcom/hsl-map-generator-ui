d43116a39f5f00620227faadea62cad51ce39a6c
Hannes Junnila <hannes.junnila@gmail.com>
2016-02-29 16:51:57 +0200

    Fix copy pasta error

--- app/components/MapScaleSelector.js
+++ app/components/MapScaleSelector.js
@@ -2,7 +2,7 @@ import React from 'react';
 import DropdownSelector from './DropdownSelector';
 
 const MapScaleSelector = ({mapScale}) => (
-  <DropdownSelector title="Tarkkuus" value={'1:' + mapScale}/>
+  <DropdownSelector title="Mittakaava" value={'1:' + mapScale}/>
 );
 
 export default MapScaleSelector;
