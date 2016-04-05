b355fe3b05633c95d70874b609e4c27e1a071c04
Hannes Junnila <hannes.junnila@gmail.com>
2016-04-05 11:00:25 +0300

    Add switching layers on and off

--- app/utils/map-utils.js
+++ app/utils/map-utils.js
@@ -1,9 +1,21 @@
-import { fromJS } from 'immutable';
+import { fromJS, Iterable } from 'immutable';
+import { find, matchesProperty } from 'lodash';
 import style from 'hsl-map-style/hsl-gl-map-with-stops-v8.json';
 
-export const baseStyle = fromJS(style);
+export const baseStyle = fromJS(style, (key, value) => {
+  var isIndexed = Iterable.isIndexed(value);
+  return isIndexed ? value.toList() : value.toOrderedMap();
+});
 
-export const styleFromLayers = (layers) => baseStyle
+export const styleFromLayers = (layers) => {
+  return baseStyle.set('layers', baseStyle.get('layers').map((layer) => {
+    const layerState = find(layers, matchesProperty('id', layer.getIn(['metadata', 'mapbox:group'])));
+    if ( !layer.get('ref') && layerState && layerState.enabled === false) {
+      return layer.setIn(['layout', 'visibility'], 'none');
+    }
+    return layer;
+  }));
+};
 
 // .set('layers', fromJS(
 //   layers
