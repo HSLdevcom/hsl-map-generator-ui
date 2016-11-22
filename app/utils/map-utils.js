d7f9dfccff2234553146bcb8a705e0ff5a5ba6cb
Sebastian <sebastian.hognabba@gmail.com>
2016-11-22 16:44:48 +0200

    Fixed eslint errors in app/utils

--- app/utils/map-utils.js
+++ app/utils/map-utils.js
@@ -7,8 +7,8 @@ export const baseStyle = fromJS(style, (key, value) => {
     return isIndexed ? value.toList() : value.toOrderedMap();
 });
 
-export const styleFromLayers = (layers, sources) => {
-    return baseStyle.mergeIn(["sources"], sources).set("layers", baseStyle.get("layers").map((layer) => {
+export const styleFromLayers = (layers, sources) =>
+    baseStyle.mergeIn(["sources"], sources).set("layers", baseStyle.get("layers").map((layer) => {
         let newLayer = layer;
         const layerState = find(layers, matchesProperty("id", newLayer.getIn(["metadata", "mapbox:group"])));
 
@@ -28,13 +28,3 @@ export const styleFromLayers = (layers, sources) => {
         }
         return newLayer;
     }));
-};
-
-// .set("layers", fromJS(
-//     layers
-//     .concat()
-//     .reverse()
-//     .filter(layer => layer.enabled)
-//     .reduce((prev, layer) => prev.concat(style.groups[layer.id].layers), [])));
-
-// TODO: This is horrible and should be replaced with a propurr implementation
