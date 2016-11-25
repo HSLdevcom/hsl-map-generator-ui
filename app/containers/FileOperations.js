391c5d07ee1777bfffad80a8bbe57aa2d219923f
Sebastian <sebastian.hognabba@gmail.com>
2016-11-25 11:35:48 +0200

    Define API_URL as process.env in webpack

--- app/containers/FileOperations.js
+++ app/containers/FileOperations.js
@@ -19,7 +19,7 @@ function mapStateToProps(state) {
         //         mapSelection: toJSON(state.mapSelection),
         //     }),
         onGenerateImage: () =>
-            fetch("http://136.243.66.163/generateImage", {
+            fetch(`${process.env.API_URL}generateImage`, {
                 method: "POST",
                 mode: "cors",
                 redirect: "follow",
@@ -35,7 +35,7 @@ function mapStateToProps(state) {
             .then(response => response.blob())
             .then(blob => saveAs(blob, "map.png")),
         onGenerateStopLabels: () =>
-            fetch("http://136.243.66.163/generateStopLabels", {
+            fetch(`${process.env.API_URL}generateStopLabels`, {
                 method: "POST",
                 mode: "cors",
                 redirect: "follow",
