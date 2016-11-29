5625bc9f5f1a92c05207dd29cb48cec1f645884f
Sebastian <sebastian.hognabba@gmail.com>
2016-11-29 09:39:43 +0200

    Moved fetch requests from FileOperation container to apiRequest action

--- app/actions/apiRequests.js
+++ app/actions/apiRequests.js
@@ -1,3 +1,7 @@
+import { toJSON } from "transit-immutable-js";
+import { styleFromLayers } from "hsl-map-generator-utils";
+import { saveAs } from "file-saver";
+
 export const GENERATE_IMAGE_REQUEST = "GENERATE_IMAGE_REQUEST";
 export const GENERATE_IMAGE_SUCCESS = "GENERATE_IMAGE_SUCCESS";
 export const GENERATE_IMAGE_ERROR = "GENERATE_IMAGE_ERROR";
@@ -14,3 +18,36 @@ export const generateImageSuccess = () => ({
 export const generateImageError = () => ({
     type: GENERATE_IMAGE_ERROR,
 });
+
+export const generateImage = state =>
+    fetch("http://localhost:8000/generateImage", {
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+        headers: new Headers({
+            "Content-Type": "application/json",
+            backend: "mapgenerator",
+        }),
+        body: JSON.stringify({
+            mapSelection: toJSON(state.mapSelection),
+            style: styleFromLayers(state.layers).toJS(),
+        }),
+    })
+    .then(response => response.blob())
+    .then(blob => saveAs(blob, "map.png"));
+
+export const generateStopLabels = state =>
+    fetch("http://localhost:8000/generateStopLabels", {
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+        headers: new Headers({
+            "Content-Type": "application/json",
+            backend: "mapgenerator",
+        }),
+        body: JSON.stringify({
+            mapSelection: toJSON(state.mapSelection),
+        }),
+    })
+    .then(response => response.blob())
+    .then(blob => saveAs(blob, "stops.html"));
--- app/containers/FileOperations.js
+++ app/containers/FileOperations.js
@@ -1,75 +1,26 @@
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-// import { ipcRenderer } from "electron";
-import { toJSON, fromJSON } from "transit-immutable-js";
-import { styleFromLayers } from "hsl-map-generator-utils";
 import { saveAs } from "file-saver";
+import { toJSON } from "transit-immutable-js";
 import FileOperations from "../components/FileOperations";
-import { loadState } from "../actions/fileOperations";
+import { generateImageRequest, generateImageSuccess, generateImageError, generateImage, generateStopLabels } from "../actions/apiRequests";
 
 function mapStateToProps(state) {
     return {
-        state,
-        // onGenerateImage: () =>
-        //     ipcRenderer.send("generateImage", {
-        //         mapSelection: toJSON(state.mapSelection),
-        //         style: styleFromLayers(state.layers).toJS(),
-        //     }),
-        // onGenerateStopLabels: () =>
-        //     ipcRenderer.send("generateStopLabels", {
-        //         mapSelection: toJSON(state.mapSelection),
-        //     }),
         onGenerateImage: () =>
-            fetch("http://localhost:8000/generateImage", {
-                method: "POST",
-                mode: "cors",
-                redirect: "follow",
-                headers: new Headers({
-                    "Content-Type": "application/json",
-                    backend: "mapgenerator",
-                }),
-                body: JSON.stringify({
-                    mapSelection: toJSON(state.mapSelection),
-                    style: styleFromLayers(state.layers).toJS(),
-                }),
-            })
-            .then(response => response.blob())
-            .then(blob => saveAs(blob, "map.png")),
-        onGenerateStopLabels: () =>
-            fetch("http://localhost:8000/generateStopLabels", {
-                method: "POST",
-                mode: "cors",
-                redirect: "follow",
-                headers: new Headers({
-                    "Content-Type": "application/json",
-                    backend: "mapgenerator",
-                }),
-                body: JSON.stringify({
-                    mapSelection: toJSON(state.mapSelection),
-                }),
-            })
-            .then(response => response.blob())
-            .then(blob => saveAs(blob, "stops.html")),
+            generateImage(state),
+        onGenerateStopLabels: () => generateStopLabels(state),
         onSaveState: () =>
             saveAs(new Blob([toJSON(state)], { type: "application/json" }), "map.json"),
     };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        onLoadState: (event) => {
-            const reader = new FileReader();
-            reader.onload = progress => dispatch(loadState(fromJSON(progress.target.result)));
-            reader.readAsText(event.target.files[0]);
-        },
-    };
+    return bindActionCreators({
+        generateImageRequest,
+        generateImageSuccess,
+        generateImageError,
+    }, dispatch);
 }
-
-// ipcRenderer.on("imageGenerated", (event, { data }) => {
-//     saveAs(new Blob([data], { type: "image/png" }), "map.png");
-// });
-//
-// ipcRenderer.on("stopLabelsGenerated", (event, { data }) => {
-//     saveAs(new Blob([data], { type: "text/html" }), "stops.html");
-// });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FileOperations);
