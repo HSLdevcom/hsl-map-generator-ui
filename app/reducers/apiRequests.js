2b80149665097380b882a9b74ed8ed463d3c69ad
Sebastian <sebastian.hognabba@gmail.com>
2016-11-29 10:00:42 +0200

    Add request counter

--- app/reducers/apiRequests.js
+++ app/reducers/apiRequests.js
@@ -1,7 +1,8 @@
-import { GENERATE_IMAGE_REQUEST, GENERATE_IMAGE_SUCCESS, GENERATE_IMAGE_ERROR, GENERATE_IMAGE_CANCEL } from "../actions/apiRequests";
+import { GENERATE_IMAGE_REQUEST, GENERATE_IMAGE_CANCEL, GENERATE_IMAGE_SUCCESS, GENERATE_IMAGE_ERROR } from "../actions/apiRequests";
 
 const initialState = {
     isLoading: false,
+    requestCounter: 0,
 };
 
 export default function apiRequests(state = initialState, action) {
@@ -9,20 +10,30 @@ export default function apiRequests(state = initialState, action) {
         case GENERATE_IMAGE_REQUEST:
             return {
                 imagePromise: action.imagePromise,
+                requestCounter: state.requestCounter + 1,
                 isLoading: true,
             };
-        case GENERATE_IMAGE_SUCCESS:
+        case GENERATE_IMAGE_SUCCESS: {
+            const openRequests = state.requestCounter - 1;
             return {
-                isLoading: false,
+                requestCounter: Math.max(openRequests, 0),
+                isLoading: openRequests > 0,
             };
-        case GENERATE_IMAGE_ERROR:
+        }
+        case GENERATE_IMAGE_ERROR: {
+            const openRequests = state.requestCounter - 1;
             return {
-                isLoading: false,
+                requestCounter: Math.max(openRequests, 0),
+                isLoading: openRequests > 0,
             };
-        case GENERATE_IMAGE_CANCEL:
+        }
+        case GENERATE_IMAGE_CANCEL: {
+            const openRequests = state.requestCounter - 1;
             return {
-                isLoading: false,
+                requestCounter: Math.max(openRequests, 0),
+                isLoading: openRequests > 0,
             };
+        }
         default:
             return state;
     }
