e768b35ed0e1a97118d8dc36a91d0fe9953e0597
Sebastian <sebastian.hognabba@gmail.com>
2017-02-13 11:01:33 +0200

    Make layers from style generation into own utils function

--- app/reducers/layers.js
+++ app/reducers/layers.js
@@ -1,25 +1,10 @@
 import update from "react/lib/update";
-import hslMapStyle from "hsl-map-style";
 import { findIndex } from "lodash";
 import { SWITCH_LAYER, TOGGLE_LAYER } from "../actions/layers";
 import { LOAD_STATE } from "../actions/fileOperations";
+import { layersFromStyle } from "../utils/map-utils";
 
-const initialState = [];
-const style = hslMapStyle.generateStyle({
-    lang: ["fi", "sv"],
-    extensions: ["icons", "stops"],
-    glyphsUrl: process.env.API_URL,
-});
-
-Object.keys(style.metadata["mapbox:groups"]).forEach((group, index) => {
-    initialState[index] = {
-        id: group,
-        enabled: style.metadata["mapbox:groups"][group].default,
-        text: style.metadata["mapbox:groups"][group].name,
-    };
-});
-
-initialState.reverse();
+const initialState = layersFromStyle();
 
 export default function layers(state = initialState, action) {
     switch (action.type) {
