a0a3663ee197670d9e20ef3290f4da150b438ea4
Hannes Junnila <hannes.junnila@gmail.com>
2017-05-04 13:08:11 +0300

    Improve pixel scale selector UX

--- app/components/PixelScaleSelector.js
+++ app/components/PixelScaleSelector.js
@@ -2,22 +2,26 @@ import React from "react";
 import DropdownSelector from "./DropdownSelector";
 import styles from "./DropdownSelector.css";
 
-const PixelScaleSelector = ({ pixelScale, toggleTab, selected, updatePixelScale }) => (
-    <DropdownSelector
-        title="Tekstin skaalaus"
-        value={`${pixelScale}x`}
-        toggleTab={toggleTab}
-        selected={selected}
-    >
-        <div className={styles.inputRow}>
-            <input
-                className={styles.input}
-                value={pixelScale}
-                onChange={event => updatePixelScale(Number(event.target.value))}
-            />
-            <div className={styles.inputUnit}>x</div>
-        </div>
-    </DropdownSelector>
-);
+const PixelScaleSelector = ({ pixelScale, toggleTab, selected, updatePixelScale }) => {
+    let input = null;
+    return (
+        <DropdownSelector
+            title="Tekstin skaalaus"
+            value={`${pixelScale}x`}
+            toggleTab={toggleTab}
+            selected={selected}
+        >
+            <div className={styles.inputRow}>
+                <input
+                    ref={(el) => { (input) = el; }}
+                    className={styles.input}
+                    defaultValue={pixelScale}
+                    onBlur={() => updatePixelScale(Number(input.value.replace(",", ".")))}
+                />
+                <div className={styles.inputUnit}>x</div>
+            </div>
+        </DropdownSelector>
+    );
+};
 
 export default PixelScaleSelector;
