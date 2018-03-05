1813891ff611ce236b5197885806477217e4c18f
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-03-05 16:33:05 +0200

    Set URLs to staging environment

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -48,8 +48,8 @@ const config = {
         new webpack.DefinePlugin({
             "process.env": {
                 NODE_ENV: JSON.stringify("development"),
-                API_URL: JSON.stringify("http://kartat.hsl.fi/"),
-                GLYPH_URL: JSON.stringify("http://kartat.hsl.fi/"),
+                API_URL: JSON.stringify("https://kartat.hsldev.com/"),
+                GLYPH_URL: JSON.stringify("https://kartat.hsldev.com/"),
             },
         }),
     ],
