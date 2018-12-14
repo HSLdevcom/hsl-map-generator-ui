d96e03eab5bcb6b0efd6b90c64e5be7139f8de1e
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-12-14 13:36:39 +0200

    Simplify button logic

--- app/components/Button.js
+++ app/components/Button.js
@@ -2,15 +2,17 @@ import React from "react";
 import classnames from "classnames";
 import styles from "./Button.css";
 
-const Button = ({ children, onClick, styleClass = "light", disabled = false }) => {
-    let classes;
-    if (disabled) classes = classnames(styles.button, styles[styleClass], styles.disabled);
-    else classes = classnames(styles.button, styles[styleClass]);
-    return (
-        <button className={classes} onClick={e => !disabled && onClick(e)}>
-            {children}
-        </button>
-    );
-};
+const Button = ({ children, onClick, styleClass = "light", disabled = false }) =>
+    <button
+        className={
+            classnames(
+                styles.button,
+                styles[styleClass],
+                disabled ? styles.disabled : null,
+            )
+        } onClick={e => !disabled && onClick(e)}
+    >
+        {children}
+    </button>;
 
 export default Button;
