b36d93028847dd5b13357fccce15efab2cb34013
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-08-21 06:51:26 +0300

    Remove glyphs url ENV variable, not used anymore.

--- app/utils/map-utils.js
+++ app/utils/map-utils.js
@@ -30,7 +30,6 @@ const mapLayers = (layers) => {
 
 export const styleFromLayers = memoize((layers, date) => {
     const style = hslMapStyle.generateStyle({
-        glyphsUrl: process.env.GLYPH_URL || "https://kartat.hsl.fi",
         components: mapLayers(layers)
     });
 
