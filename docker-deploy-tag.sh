555b9267be64c63993b51a945c3bdf5b204c3ccf
Anton <anton.jyrkiainen@helsinki.fi>
2020-02-14 16:02:08 +0200

    tag changed

--- docker-deploy-tag.sh
+++ docker-deploy-tag.sh
@@ -8,7 +8,7 @@ read -p "Tag: " TAG
 
 DOCKER_TAG=${TAG:-production}
 DOCKER_IMAGE=$ORG/hsl-map-generator-ui:${DOCKER_TAG}
-DOCKER_IMAGE_CYPRESS=ahjyrkia/generator-ui-e2e:${DOCKER_TAG}
+DOCKER_IMAGE_CYPRESS=$ORG/generator-ui-e2e:${DOCKER_TAG}
 
 docker build --build-arg BUILD_ENV=${TAG:-production} -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE
