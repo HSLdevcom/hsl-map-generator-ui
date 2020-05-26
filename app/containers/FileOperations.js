2ed43566c710e381710df4948b63a4b9fa398625
Anton <anton.jyrkiainen@helsinki.fi>
2020-05-26 11:51:57 +0300

    only saving necessary attributes of viewport

--- app/containers/FileOperations.js
+++ app/containers/FileOperations.js
@@ -6,13 +6,29 @@ import FileOperations from "../components/FileOperations";
 import {generateImage} from "../actions/apiRequests";
 import {toggleSaveWorldFile} from "../actions/settings";
 import {loadState} from "../actions/fileOperations";
+import {version} from "os";
 
 function mapStateToProps(state) {
     return {
         ...state.settings,
         onSaveState: () => {
+            const config = {};
+            Object.keys(state).forEach((attribute) => {
+                if (attribute === "viewport") {
+                    config[attribute] = {
+                        altitude: state.viewport.altitude,
+                        bearing: state.viewport.bearing,
+                        height: state.viewport.height,
+                        latitude: state.viewport.latitude,
+                        longitude: state.viewport.longitude,
+                        zoom: state.viewport.zoom
+                    };
+                    return;
+                }
+                config[attribute] = state[attribute];
+            });
             saveAs(
-                new Blob([toJSON(state)], {type: "application/json"}),
+                new Blob([toJSON(config)], {type: "application/json"}),
                 "map.json"
             );
         }
