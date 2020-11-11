6c72e42f135b376e59112c6971687077045852b9
Anton <anton.jyrkiainen@helsinki.fi>
2020-11-11 15:31:03 +0200

    show time spent generating posters

--- app/components/ShowListModalItem.js
+++ app/components/ShowListModalItem.js
@@ -6,6 +6,8 @@ import styles from "./ShowListModalItem.css";
 import {downloadPoster} from "../utils/publisher-api";
 import Button from "./Button";
 
+const FINISHED_MESSAGE = "Rendered successfully";
+
 function getColorFromStatus(status) {
     switch (status) {
         case "READY":
@@ -19,44 +21,77 @@ function getColorFromStatus(status) {
     }
 }
 
-const ShowListModalItem = ({item, openLogId, openLog, closeLog}) => (
-    <div className={styles.container}>
-        <div className={styles.text} data-cy={item.props.configuration.name}>
-            <span className={styles.itemName}>
-                {item.props.configuration.name && item.props.configuration.name}
-                {!item.props.configuration.name && "- Nimetön -"}
-            </span>
-            <span
-                className={styles.pill}
-                style={{backgroundColor: getColorFromStatus(item.status)}}>
-                {item.status}
-            </span>
-            <br />
-            Lisätty: {moment(item.createdAt).format("D.M.YYYY HH:mm")}
-            <br />
-            Kohde päivämäärä:{" "}
-            {moment(item.props.configuration.date).format("D.M.YYYY")}
+function getBuildTime(item) {
+    if (item.status != "READY") return;
+    const finishedEvent = item.events.filter(
+        (event) => event.message === FINISHED_MESSAGE
+    );
+    const created = moment(item.createdAt);
+    const finished = moment(finishedEvent[0].createdAt);
+    const timeDiff = finished.diff(created);
+    const tempTime = moment.duration(timeDiff);
+    return {
+        hours: tempTime.hours(),
+        minutes: tempTime.minutes(),
+        seconds: tempTime.seconds()
+    };
+}
+
+function getTime(buildTime) {
+    if (!buildTime) return;
+    const hours = buildTime.hours != 0 ? `${buildTime.hours}h ` : "";
+    const minutes = buildTime.minutes != 0 ? `${buildTime.minutes}min ` : "";
+    const seconds = buildTime.seconds != 0 ? `${buildTime.seconds}s ` : "";
+    return `${hours}${minutes}${seconds}`;
+}
+
+const ShowListModalItem = ({item, openLogId, openLog, closeLog}) => {
+    const buildTime = getBuildTime(item);
+    const time = getTime(buildTime);
+    return (
+        <div className={styles.container}>
+            <div
+                className={styles.text}
+                data-cy={item.props.configuration.name}>
+                <span className={styles.itemName}>
+                    {item.props.configuration.name &&
+                        item.props.configuration.name}
+                    {!item.props.configuration.name && "- Nimetön -"}
+                </span>
+                <span
+                    className={styles.pill}
+                    style={{backgroundColor: getColorFromStatus(item.status)}}>
+                    {item.status}
+                </span>
+                <br />
+                Lisätty: {moment(item.createdAt).format("D.M.YYYY HH:mm")}
+                <br />
+                Kohde päivämäärä:{" "}
+                {moment(item.props.configuration.date).format("D.M.YYYY")}
+                {time ? <br /> : ""}
+                {time ? `Generointiaika: ${time}` : ""}
+            </div>
+            <div className={styles.buttons}>
+                <Button
+                    onClick={() => downloadPoster({id: item.id})}
+                    disabled={item.status !== "READY"}>
+                    Lataa
+                </Button>
+                <Button onClick={() => openLog(item.id)}>Log</Button>
+                <Modal
+                    isOpen={openLogId && openLogId === item.id}
+                    onRequestClose={closeLog}>
+                    <div className={styles.log}>
+                        <h3>Log</h3>
+                        {item.events.map((event) => (
+                            <p key={event.createdAt}>{event.message}</p>
+                        ))}
+                        <Button onClick={closeLog}>Sulje</Button>
+                    </div>
+                </Modal>
+            </div>
         </div>
-        <div className={styles.buttons}>
-            <Button
-                onClick={() => downloadPoster({id: item.id})}
-                disabled={item.status !== "READY"}>
-                Lataa
-            </Button>
-            <Button onClick={() => openLog(item.id)}>Log</Button>
-            <Modal
-                isOpen={openLogId && openLogId === item.id}
-                onRequestClose={closeLog}>
-                <div className={styles.log}>
-                    <h3>Log</h3>
-                    {item.events.map((event) => (
-                        <p key={event.createdAt}>{event.message}</p>
-                    ))}
-                    <Button onClick={closeLog}>Sulje</Button>
-                </div>
-            </Modal>
-        </div>
-    </div>
-);
+    );
+};
 
 export default ShowListModalItem;
