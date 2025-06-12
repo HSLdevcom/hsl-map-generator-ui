6da5159dc3074555d3089613efc91ea0aa0537a6
e-halinen <54105602+e-halinen@users.noreply.github.com>
2025-06-12 13:05:22 +0300

    Push login provider changes (#143)
    * 25226: react-map-gl switched to maplibre-gl (#130)
    
    * react-map-gl switched to maplibre-gl
    
    * lint fixes
    
    * more lint errors fixed
    
    * zoneSymbols bit more responsive
    
    * build and style fix
    
    * yarn.lock update
    
    * AB#28974: Represent zone symbols correctly in UI in regards to prints
    
    * zone symbol sizing
    
    * lint fixes
    
    * resize symbols on all zoom events
    
    * Bump hsl-map-style version to 1.1.3 (#133)
    
    * AB#35806: Fix crashing coordinate input fields (#134)
    
    * AB#48634: Bump hsl-map-style to 1.2.0 (#135)
    
    * WIP: AB#34322: Fix marker glitching when dragging stops (#136)
    
    * AB#53258: Fix map generator appearing in google search results (#138)
    
    * AB#56575: Add version numbering and check against prod (#140)
    
    * AB#56568: Change license to AGPL-3.0-only (#141)
    
    * React updates (#132)
    
    * react updates
    
    * lint fixes
    
    * v1.1.0
    
    * Update login providers, make login provider configurable
    
    ---------
    
    Co-authored-by: Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>

--- app/components/Login.js
+++ app/components/Login.js
@@ -21,7 +21,7 @@ const LightTooltip = withStyles((theme) => ({
 
 const openLoginForm = () => {
     window.location.replace(
-        `https://hslid-uat.cinfra.fi/openid/auth?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code&scope=email+https://oneportal.trivore.com/scope/groups.readonly`
+        `${process.env.REACT_APP_LOGIN_PROVIDER_URI}/openid/auth?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code&scope=email+https://oneportal.trivore.com/scope/groups.readonly`
     );
 };
 
